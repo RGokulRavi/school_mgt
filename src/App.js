f805d89ee91cdf3dd92d70a86636dd4cb9dbc736
RGokulRavi <kstgokulr@gmail.com>
2023-07-07 18:31:22 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import ResponsiveAppBar from "./components/navBar";
+import StudentRegistration from "./components/studentRegistration";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<ResponsiveAppBar />} />
+        <Route path="/students" element={<StudentRegistration />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/studentRegistration.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentRegistration = () => {
+  return <div className="StudentRegistration">StudentRegistration</div>;
+};
+
+export default StudentRegistration;
